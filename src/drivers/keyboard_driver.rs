6e0b6bdafc1f0f234c55fe383bfb90bb43172013
Starr Horne <starr@chromahq.com>
2017-06-22 19:31:15 -0700

    convert keyboard keys to chip8 keypad flags

--- src/drivers/keyboard_driver.rs
+++ src/drivers/keyboard_driver.rs
@@ -1,7 +1,6 @@
 use sdl2;
 use sdl2::event::Event;
 use sdl2::keyboard::Keycode;
-use std::collections::HashSet;
 use std;
 
 pub struct KeyboardDriver {
@@ -15,9 +14,9 @@ impl KeyboardDriver {
         }
     }
 
+
     pub fn poll(&mut self)  {
 
-        let mut prev_keys = HashSet::new();
 
         'running: loop {
             for event in self.events.poll_iter() {
@@ -26,18 +25,45 @@ impl KeyboardDriver {
                 };
             }
 
-            // Create a set of pressed Keys.
-            let keys = self.events.keyboard_state().pressed_scancodes().filter_map(Keycode::from_scancode).collect();
+            let keys: Vec<Keycode> = self.events.keyboard_state()
+                .pressed_scancodes()
+                .filter_map(Keycode::from_scancode)
+                .collect();
 
-            // Get the difference between the new and old sets.
-            let new_keys = &keys - &prev_keys;
-            let old_keys = &prev_keys - &keys;
+            let mut chip8_keys = [false; 16];
 
-            if !new_keys.is_empty() || !old_keys.is_empty() {
-                println!("new_keys: {:?}\told_keys:{:?}", new_keys, old_keys);
+            for key in keys {
+                let index = match key {
+                    Keycode::Num1 => Some(0x1),
+                    Keycode::Num2 => Some(0x2),
+                    Keycode::Num3 => Some(0x3),
+                    Keycode::Num4 => Some(0xc),
+                    Keycode::Q    => Some(0x4),
+                    Keycode::W    => Some(0x5),
+                    Keycode::E    => Some(0x6),
+                    Keycode::R    => Some(0xd),
+                    Keycode::A    => Some(0x7),
+                    Keycode::S    => Some(0x8),
+                    Keycode::D    => Some(0x9),
+                    Keycode::F    => Some(0xe),
+                    Keycode::Z    => Some(0xa),
+                    Keycode::X    => Some(0x0),
+                    Keycode::C    => Some(0xb),
+                    Keycode::V    => Some(0xf),
+                    _             => None,
+                };
+
+                if let Some(i) = index {
+                    chip8_keys[i] = true;
+                }
+                    
             }
 
-            prev_keys = keys;
+            println!("{:?}", chip8_keys);
+
+
+
+            // prev_keys = keys;
 
             std::thread::sleep(std::time::Duration::from_millis(100));
         }
