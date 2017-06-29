7b7b2b01c8fd0a6b88c0bd2b6b3b640367432275
Starr Horne <starr@chromahq.com>
2017-06-29 15:32:16 -0700

    add comments;

--- src/processor.rs
+++ src/processor.rs
@@ -189,63 +189,80 @@ impl Processor {
 
     }
     // RET:  Return from a subroutine.
+    // The interpreter sets the program counter to the address at the
+    // top of the stack, then subtracts 1 from the stack pointer.
     fn op_00ee(&mut self) -> ProgramCounter {
         self.sp -= 1;
         ProgramCounter::Jump(self.stack[self.sp])
     }
     // JP addr
+    // The interpreter sets the program counter to nnn.
     fn op_1nnn(&mut self, nnn: usize) -> ProgramCounter {
         ProgramCounter::Jump(nnn)
     }
     // CALL addr
+    // The interpreter increments the stack pointer, then puts the
+    // current PC on the top of the stack. The PC is then set to nnn.
     fn op_2nnn(&mut self, nnn: usize) -> ProgramCounter {
         self.stack[self.sp] = self.pc;
         self.sp += 1;
         ProgramCounter::Jump(nnn)
     }
-    // SE Vx, byte:  Skip next instruction if Vx = kk.
+    // SE Vx, byte:
+    // Skip next instruction if Vx = kk.
     fn op_3xkk(&mut self, x: usize, kk: u8) -> ProgramCounter {
         ProgramCounter::skip_if(self.v[x] == kk)
     }
-    // SNE Vx, byte. Skip next instruction if Vx != kk.
+    // SNE Vx, byte.
+    // Skip next instruction if Vx != kk.
     fn op_4xkk(&mut self, x: usize, kk: u8) -> ProgramCounter {
         ProgramCounter::skip_if(self.v[x] != kk)
     }
     // SE Vx, Vy
+    // Skip next instruction if Vx = Vy.
     fn op_5xy0(&mut self, x: usize, y: usize) -> ProgramCounter {
         ProgramCounter::skip_if(self.v[x] == self.v[y])
     }
     // LD Vx, byte
+    // Set Vx = kk.
     fn op_6xkk(&mut self, x: usize, kk: u8) -> ProgramCounter {
         self.v[x] = kk;
         ProgramCounter::Next
     }
     // ADD Vx, byte
+    // Set Vx = Vx + kk.
     fn op_7xkk(&mut self, x: usize, kk: u8) -> ProgramCounter {
         self.v[x] += kk;
         ProgramCounter::Next
     }
     // LD Vx, Vy
+    // Set Vx = Vy.
     fn op_8xy0(&mut self, x: usize, y: usize) -> ProgramCounter {
         self.v[x] = self.v[y];
         ProgramCounter::Next
     }
     // OR Vx, Vy
+    // Set Vx = Vx OR Vy.
     fn op_8xy1(&mut self, x: usize, y: usize) -> ProgramCounter {
         self.v[x] |= self.v[y];
         ProgramCounter::Next
     }
     // AND Vx, Vy
+    // Set Vx = Vx AND Vy.
     fn op_8xy2(&mut self, x: usize, y: usize) -> ProgramCounter {
         self.v[x] &= self.v[y];
         ProgramCounter::Next
     }
     // XOR Vx, Vy
+    // Set Vx = Vx XOR Vy.
     fn op_8xy3(&mut self, x: usize, y: usize) -> ProgramCounter {
         self.v[x] ^= self.v[y];
         ProgramCounter::Next
     }
     // ADD Vx, Vy
+    // The values of Vx and Vy are added together. If the result is
+    // greater than 8 bits (i.e., > 255,) VF is set to 1, otherwise 0.
+    // Only the lowest 8 bits of the result are kept, and stored in Vx.
     fn op_8xy4(&mut self, x: usize, y: usize) -> ProgramCounter {
         let vx = self.v[x] as u16;
         let vy = self.v[y] as u16;
@@ -255,55 +272,68 @@ impl Processor {
         ProgramCounter::Next
     }
     // SUB Vx, Vy
+    // If Vx > Vy, then VF is set to 1, otherwise 0. Then Vy is subtracted from Vx, and the results stored in Vx.
     fn op_8xy5(&mut self, x: usize, y: usize) -> ProgramCounter {
         self.v[0x0f] = if self.v[x] > self.v[y] { 1 } else { 0 };
         self.v[x] = self.v[x].wrapping_sub(self.v[y]);
         ProgramCounter::Next
     }
     // SHR Vx {, Vy}
+    // If the least-significant bit of Vx is 1, then VF is set to 1,
+    // otherwise 0. Then Vx is divided by 2.
     fn op_8x06(&mut self, x: usize) -> ProgramCounter {
-        self.v[0x0f] = self.v[x] & 0x01;
-        self.v[x] = self.v[x] >> 1;
+        self.v[0x0f] = self.v[x] & 1;
+        self.v[x] /= 2;
         ProgramCounter::Next
     }
     // SUBN Vx, Vy
+    // If Vy > Vx, then VF is set to 1, otherwise 0. Then Vx is subtracted
+    // from Vy, and the results stored in Vx.
     fn op_8xy7(&mut self, x: usize, y: usize) -> ProgramCounter {
         self.v[0x0f] = if self.v[y] > self.v[x] { 1 } else { 0 };
         self.v[x] = self.v[y].wrapping_sub(self.v[x]);
         ProgramCounter::Next
     }
     // SHL Vx {, Vy}
+    // If the most-significant bit of Vx is 1, then VF is set to 1,
+    // otherwise to 0. Then Vx is multiplied by 2.
     fn op_8x0e(&mut self, x: usize) -> ProgramCounter {
         self.v[0x0f] = (self.v[x] & 0b10000000) >> 7;
-        self.v[x] = self.v[x] << 1;
+        self.v[x] *= 2;
         ProgramCounter::Next
     }
-
-
     // SNE Vx, Vy
+    // Skip next instruction if Vx != Vy.
     fn op_9xy0(&mut self, x: usize, y: usize) -> ProgramCounter {
         ProgramCounter::skip_if(self.v[x] != self.v[y])
     }
-
     // LD I, addr
+    // Set I = nnn.
     fn op_annn(&mut self, nnn: usize) -> ProgramCounter {
         self.i = nnn;
         ProgramCounter::Next
     }
-
     // JP V0, addr
+    // The program counter is set to nnn plus the value of V0.
     fn op_bnnn(&mut self, nnn: usize) -> ProgramCounter {
         ProgramCounter::Jump((self.v[0] as usize) + nnn)
     }
-
     // RND Vx, byte
+    // The interpreter generates a random number from 0 to 255,
+    // which is then ANDed with the value kk. The results are stored in Vx.
     fn op_cxkk(&mut self, x: usize, kk: u8) -> ProgramCounter {
         let mut rng = rand::thread_rng();
         self.v[x] = rng.gen::<u8>() & kk;
         ProgramCounter::Next
     }
-
-    // DRW Vx, Vy, nibble
+    // DRW Vx, Vy, n
+    // The interpreter reads n bytes from memory, starting at the address
+    // stored in I. These bytes are then displayed as sprites on screen at
+    // coordinates (Vx, Vy). Sprites are XORed onto the existing screen.
+    // If this causes any pixels to be erased, VF is set to 1, otherwise
+    // it is set to 0. If the sprite is positioned so part of it is outside
+    // the coordinates of the display, it wraps around to the opposite side
+    // of the screen.
     fn op_dxyn(&mut self, x: usize, y: usize, n: usize) -> ProgramCounter {
         self.v[0x0f] = 0;
         for byte in 0..n {
@@ -319,50 +349,58 @@ impl Processor {
         self.vram_changed = true;
         ProgramCounter::Next
     }
-
     // SKP Vx
+    // Skip next instruction if key with the value of Vx is pressed.
     fn op_ex9e(&mut self, x: usize) -> ProgramCounter {
         ProgramCounter::skip_if(self.keypad[self.v[x] as usize])
     }
-
     // SKNP Vx
+    // Skip next instruction if key with the value of Vx is NOT pressed.
     fn op_exa1(&mut self, x: usize) -> ProgramCounter {
         ProgramCounter::skip_if(!self.keypad[self.v[x] as usize])
     }
     // LD Vx, DT
+    // Set Vx = delay timer value.
     fn op_fx07(&mut self, x: usize) -> ProgramCounter {
         self.v[x] = self.delay_timer;
         ProgramCounter::Next
     }
     // LD Vx, K
+    // Wait for a key press, store the value of the key in Vx.
     fn op_fx0a(&mut self, x: usize) -> ProgramCounter {
         self.keypad_waiting = true;
         self.keypad_register = x;
         ProgramCounter::Next
     }
-
     // LD DT, Vx
+    // Set delay timer = Vx.
     fn op_fx15(&mut self, x: usize) -> ProgramCounter {
         self.delay_timer = self.v[x];
         ProgramCounter::Next
     }
     // LD ST, Vx
+    // Set sound timer = Vx.
     fn op_fx18(&mut self, x: usize) -> ProgramCounter {
         self.sound_timer = self.v[x];
         ProgramCounter::Next
     }
     // ADD I, Vx
+    // Set I = I + Vx
     fn op_fx1e(&mut self, x: usize) -> ProgramCounter {
         self.i += self.v[x] as usize;
         ProgramCounter::Next
     }
     // LD F, Vx
+    // Set I = location of sprite for digit Vx.
     fn op_fx29(&mut self, x: usize) -> ProgramCounter {
         self.i = (self.v[x] as usize) * 5;
         ProgramCounter::Next
     }
 
     // LD B, Vx
+    // The interpreter takes the decimal value of Vx, and places
+    // the hundreds digit in memory at location in I, the tens digit
+    // at location I+1, and the ones digit at location I+2.
     fn op_fx33(&mut self, x: usize) -> ProgramCounter {
         self.ram[self.i] = self.v[x] / 100;
         self.ram[self.i + 1] = (self.v[x] % 100) / 10;
@@ -371,6 +409,8 @@ impl Processor {
     }
 
     // LD [I], Vx
+    // The interpreter copies the values of registers V0 through Vx
+    // into memory, starting at the address in I.
     fn op_fx55(&mut self, x: usize) -> ProgramCounter {
         for i in 0..self.v[x] as usize {
             self.ram[self.i + i] = self.v[i];
@@ -379,6 +419,8 @@ impl Processor {
     }
 
     // LD Vx, [I]
+    // The interpreter reads values from memory starting at location
+    // I into registers V0 through Vx.
     fn op_fx65(&mut self, x: usize) -> ProgramCounter {
         for i in 0..self.v[x] as usize {
             self.v[i] = self.ram[self.i + i];
